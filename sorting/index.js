62cdf3cb7997bf6fc51aa147171313e0f6e0d388
Raj Amancha <therajamancha@gmail.com>
2025-04-20 18:58:05 +0530

    select sort algorithm

--- sorting/index.js
+++ sorting/index.js
@@ -1,5 +1,5 @@
 // bubble sort
-
+/* 
 let data = [40, 30, 12, 25];
 
 for (let i = 0; i < data.length; i++) {
@@ -13,3 +13,23 @@ for (let i = 0; i < data.length; i++) {
 }
 
 console.log(data);
+ */
+
+// select sort
+/* 
+let data = [20, 12, 53, 3];
+
+for (let i = 0; i < data.length; i++) {
+  let minId = i;
+  for (let j = i + 1; j < data.length; j++) {
+    if (data[j] < data[minId]) {
+      minId = j;
+    }
+  }
+  let temp = data[minId];
+  data[minId] = data[i];
+  data[i] = temp;
+}
+
+console.log(data);
+ */
